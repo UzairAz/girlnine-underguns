044ac9d7b620adf2f173bbb0de7d49cd06d8b9be
UzairAz <98876103+UzairAz@users.noreply.github.com>
2022-12-05 15:59:42 +0500

    Add files via upload

---
+++ pos_sale_report/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "POS Sale Report",
+    'summary': """POS Sale Report Extended""",
+    'description': """
+        POS Sale Report Extend
+    """,
+    'author': "BlueEast",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'point_of_sale'],
+    'data': [
+        'views/views.xml',
+    ],
+}
